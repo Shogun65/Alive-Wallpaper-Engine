a52cbd205199833a7b562ae42d958add386e282d
Chahat <arceusyt20@gmail.com>
2026-03-26 11:51:47 +0530

    Add FFmpeg files and include thats it.

---
+++ FFmpeg.h
@@ -0,0 +1,27 @@
+#pragma once
+
+extern "C" {
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <libavutil/avutil.h>
+#include <libavutil/imgutils.h>
+#include <libavutil/opt.h>
+#include <libswscale/swscale.h>
+#include <libswresample/swresample.h>
+}
+
+class FFmpeg
+{
+public:
+
+
+
+
+
+
+
+
+private:
+
+
+};
