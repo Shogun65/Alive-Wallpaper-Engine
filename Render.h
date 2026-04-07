e3924cfb3a4d7033cd36984afdc42be7eae7ba8f
Chahat <arceusyt20@gmail.com>
2026-04-07 12:18:29 +0530

    Add Render.inl Didn't code anythink for now

--- Render.h
+++ Render.h
@@ -7,13 +7,21 @@ class Render
 {
 public:
 
+	// this is a test func
 	void cleanscreen(ID3D11RenderTargetView* RTVOfBackBuffer, 
 		IDXGISwapChain1* swapchin1, 
 		ID3D11DeviceContext* devicecontext);
 
-
+	template<typename FramePOPFunc, typename FrameReturnFunc>
+	void RenderFrame(
+		ID3D11RenderTargetView* RTVOfBackBuffer,
+		IDXGISwapChain1* swapchin1,
+		ID3D11DeviceContext* devicecontext,
+		FramePOPFunc FramePOP,
+		FrameReturnFunc FrameReturn);
 
 
 private:
 	//idk
 };
+#include "Render.inl"
---
+++ Render.inl
@@ -0,0 +1,12 @@
+template<typename FramePOPFunc, typename FrameReturnFunc>
+void Render::RenderFrame(
+	ID3D11RenderTargetView* RTVOfBackBuffer,
+	IDXGISwapChain1* swapchin1,
+	ID3D11DeviceContext* devicecontext,
+	FramePOPFunc FramePOP,
+	FrameReturnFunc FrameReturn)
+{
+
+
+
+}
